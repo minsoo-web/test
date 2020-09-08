867c0079c0647fe400b2eb847540a5cb882f1e14
Minsoo <zlemzlem5656@naver.com>
2020-09-08 10:11:09 +0900

    테스트 불필요

--- README.md
+++
@@ -1,3 +0,0 @@
-# IRIS-E2E-SAAS
-
-[End-to-End Testing for IRIS landing page](http://192.168.102.136:8083/main/main-landing)
--- script/modules/side_configure/user_manager.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-class UserChanger(object):
-    def change(self, new_user: str, new_password: str, data: str) -> str:
-        data = re.sub('DEFAULT_USER', new_user, data)
-        return re.sub('DEFAULT_PASSWORD', new_password, data)
